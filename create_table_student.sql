82994ce290b23beb0e4ee1e20afdbc9aac521479
redperalta23 <78672503+redperalta23@users.noreply.github.com>
2023-11-28 21:39:07 +0800

    created students table and inserted values

---
+++ create_table_student.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Students (
+	StudentID INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(100),
+    DateOfBirth DATE,
+    GradeLevel INT,
+    PRIMARY KEY (StudentID)
+    );
+    
---
+++ insert_into_students.sql
@@ -0,0 +1,11 @@
+INSERT INTO Students ( Name, DateOfBirth, GradeLevel )
+VALUES ( 'Charles Xavier', '1965-02-14', 12 ),
+( 'Erik Magnus', '1963-03-21', 12 ),
+( 'Henry Philip McCoy', '1980-09-16', 10 ),
+( 'Raven Darkholme', '1970-10-25', 10 ),
+( 'James Logan Howlett', '1940-11-10', 11 ),
+( 'Jean Grey', '1985-08-09', 13 ),
+( 'Scott Summers', '1983-07-24', 10 ),
+( 'Anna Rogue Marie', '1995-12-03', 10 ),
+( 'Ororo Storm Munroe', '1988-06-15', 10 ),
+( 'Kurt Wagner', '1982-05-27', 10 );
