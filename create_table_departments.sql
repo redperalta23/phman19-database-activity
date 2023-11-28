5751601fc654bfbe89091693e5a572cc019fdc2d
redperalta23 <78672503+redperalta23@users.noreply.github.com>
2023-11-28 21:57:41 +0800

    created departments table and inserted values

---
+++ create_table_departments.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Departments (
+DepartmentID INT NOT NULL AUTO_INCREMENT,
+DepartmentName VARCHAR(100),
+TeacherID INT,
+PRIMARY KEY (DepartmentID)
+);
---
+++ insert_into_departments.sql
@@ -0,0 +1,10 @@
+INSERT INTO Departments ( DepartmentName, TeacherID )
+VALUES ( 'IT', 5 ),
+( 'ENGINEERING', 3 ),
+( 'SCIENCES', 2 ),
+( 'LAW', 1 ),
+( 'EDUCATION', 10 ),
+( 'NURSING', 4 ),
+( 'ADMINISTRATION', 6 ),
+( 'MEDICINE', 7 ),
+( 'GRADUATE SCHOOL', 8);
