b1149858555dc1f042a7035969d54750c575b412
redperalta23 <78672503+redperalta23@users.noreply.github.com>
2023-11-28 21:42:16 +0800

    created teachers database and inserted values

---
+++ insert_into_teachers.sql
@@ -0,0 +1,11 @@
+INSERT INTO Teachers ( Name )
+VALUES ( 'Albert Einstein' ),
+( 'Marie Curie' ),
+( 'Alexander Graham Bell' ),
+( 'Bill Gates' ),
+( 'Mark Zuckerberg' ),
+( 'Elon Musk' ),
+( 'Ada Lovelace' ),
+( 'Guido Van Rossum' ),
+( 'Linus Torvalds' ),
+( 'Jose Rizal' );
